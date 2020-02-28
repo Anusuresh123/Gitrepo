2e81dccf6732e83623c04b9aa51c5f9a929aa127
Anusuresh123 <48363757+Anusuresh123@users.noreply.github.com>
2020-02-28 23:10:28 +0530

    Update SamplecommitProgram.java

--- Gitproject/src/gitPackage/SamplecommitProgram.java
+++ Gitproject/src/gitPackage/SamplecommitProgram.java
@@ -5,9 +5,11 @@ public class SamplecommitProgram {
 	public static void main(String[] args) {
 
 		
-		int i=1, k=1;
+		int i=1, j=0;k=1;
 		int c=i+k;
-				System.out.println(i);
+		System.out.println(i);
+		System.out.println(j);
+                System.out.println(k);
 
 		System.out.println(c);
 
