6e66ecc298778537e4ba067ab88270b6f4b1a1a5
Anusuresh123 <p.anusuresh0009@gmail.com>
2020-02-28 22:48:17 +0530

    gitProgram2

---
+++ Gitproject/src/gitPackage/Gitprogram2.java
@@ -0,0 +1,22 @@
+package gitPackage;
+
+public class Gitprogram2 {
+
+	public static void main(String[] args) {
+
+
+		
+		
+		
+		int i= 4, j=2, sum;
+		sum=i+j;
+		
+		
+		System.out.println("the value is: " +sum);
+		
+		
+	}
+	
+
+	}
+
